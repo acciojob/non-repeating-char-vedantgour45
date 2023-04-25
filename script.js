bfe95581bce3ca4cb4003cf460c20f4bc4df7123
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-04-25 18:45:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+
+function firstChar(text) {
+  // your code here
+  const charMap = {}; 
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
+  }
+
+   for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    if (charMap[char] === 1) {
+      return char;
+    }
+  }
+  return null; 
+}
+
+const text = prompt("Enter text:");
+alert(firstChar(text)); 
